2c935dc85377352d1faf6a11293bb78fb9e8069f
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2022-09-28 19:14:26 +0200

    Ferdig med oppgave1 i Oblig1

--- src/oblig1/Oppgave1.java
+++
@@ -1,8 +0,0 @@
-package oblig1;
-
-public class Oppgave1 {
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ src/oblig1/oblig1Oppgave1.java
@@ -0,0 +1,41 @@
+package oblig1;
+class sirkel{
+    //Lager static metode som beregner og returner sirkelens diameter.
+    public static double diameter(double r){
+        double diamter= r*2;
+        return diamter;}
+
+    //lager static metode som beregner og returner sirkelens omkrets
+    public static double omkrets(double r){
+        double omkrets=2*Math.PI*r;
+        return omkrets;}
+
+    //Lager static metode som beregner og returner sirkelens Areal
+    public static double areal(double r){
+        double areal=Math.PI*Math.pow(r,2);
+        return areal;}
+    public static void ut(double r){
+        System.out.println("Areal av sirkel radius 23.5 er "+String.format("%.2f",sirkel.areal(r)));
+        System.out.println("Omkrets av sirkel radius 23.5 er "+String.format("%.2f",sirkel.omkrets(r)));
+        System.out.println("Diameter til sirkel radius 23.5 er "+String.format("%.2f",sirkel.diameter(r)));
+
+    }
+
+
+}
+
+
+
+public class oblig1Oppgave1{
+    public static void main(String[] args) {
+        //Setter en variabel lik en verdi for radius
+        double r=23.5;
+
+        sirkel nySirkel=new sirkel();
+        sirkel.ut(r);
+
+
+
+    }
+
+}
